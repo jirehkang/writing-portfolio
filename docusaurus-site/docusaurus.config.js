d915be7856fa4634dfaa79bcd1cbb528d202b70b
jkayk <jirehkang10@gmail.com>
2025-04-06 18:39:34 +0900

    edit config for github page deployment

--- docusaurus-site/docusaurus.config.js
+++ docusaurus-site/docusaurus.config.js
@@ -10,21 +10,22 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "My Site",
-	tagline: "Dinosaurs are cool",
+	title: "Jireh's Writing Portfolio",
+	tagline: "Technical Writing, UX Writing, and More",
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://jirehkang.netlify.app",
+	url: "https://jirehkang.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/writing-portfolio/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
-
+	organizationName: "jirehkang", // Usually your GitHub org/user name.
+	projectName: "writing-portfolio", // Usually your repo name.
+	trailingSlash: false,
+	deploymentBranch: "gh-pages",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
 
