0aa80e703ca9f20121e3c3ade922a1302beb4c11
jirehkang <jireh.k.kang@gmail.com>
2025-04-10 23:42:29 +0900

    add ScrollIcon

--- src/components/ParallaxSection/index.js
+++ src/components/ParallaxSection/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import styles from "./ParallaxSection.module.css";
+import ScrollIcon from "../ScrollIcon";
 
 export default function ParallaxSection() {
 	const alignCenter = { display: "flex", alignItems: "center" };
@@ -22,6 +23,14 @@ export default function ParallaxSection() {
 							<p className={styles.jobTitle}>Writer</p>
 						</div>
 					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={0.35}
+					speed={0.5}
+					style={{ ...alignCenter, justifyContent: "center" }}
+				>
+					<ScrollIcon></ScrollIcon>
 				</ParallaxLayer>
 
 				<ParallaxLayer
