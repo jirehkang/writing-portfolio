b94e953574fb34099761c575fbc0f5d33aeb7a5e
jirehkang <jireh.k.kang@gmail.com>
2025-04-13 16:09:00 +0900

    delete default files and change scroll animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,6 @@ import Layout from "@theme/Layout";
 import IntroSection from "@site/src/components/IntroSection";
 import ProjectsSection from "@site/src/components/ProjectsSection";
 import ContactSection from "@site/src/components/ContactSection";
-// import ParallaxSection from "@site/src/components/ParallaxSection";
-// import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
-import Heading from "@theme/Heading";
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<Heading as="h1" className="hero__title">
-					{siteConfig.title}
-				</Heading>
-				<p className="hero__subtitle">{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="/docs/intro"
-					>
-						Docusaurus Tutorial - 5min ⏱️
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
-}
 
 export default function Home() {
 	const { siteConfig } = useDocusaurusContext();
